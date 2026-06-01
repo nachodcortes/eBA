8eb10e03e880ef908c284a7bf867bbae9077334c
Sofia Rubachin <102734675+sofiruba@users.noreply.github.com>
2026-06-01 18:26:06 -0300

    Delete frontend/src/config/api.ts

--- frontend/src/config/api.ts
+++
@@ -1,9 +0,0 @@
-<<<<<<< Updated upstream
-export const API_URL = "http://192.168.68.103:3000";
-=======
-export const API_URL = "http://172.20.10.14:3000";
->>>>>>> Stashed changes
-// si estas en celular pones "ipconfig getifaddr en0" en consola y esa ip la usas aca, 
-// si estas en emulador de android podes usar 10.0.2.2
-// en windows podes usar "ipconfig" y buscar la ip de tu red local, y esa ip la usas aca
-// en uade haces lo mismo, si ves la app de web podes usar "http://localhost:3000" s
