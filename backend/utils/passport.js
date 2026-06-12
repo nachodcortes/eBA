78d4f810c580f2a83c005b807fa7bdef85df4f28
Sofia Rubachin <102734675+sofiruba@users.noreply.github.com>
2026-06-12 13:52:10 -0300

    Update passport.js

--- backend/utils/passport.js
+++ backend/utils/passport.js
@@ -33,6 +33,7 @@ passport.use(
         usuario = new Usuario({
           googleId: profile.id,
           nombre: profile.displayName,
+          nombreUsuario: profile.emails[0].value.split("@")[0].toLowerCase(),
           email: profile.emails[0].value,
           fotoPerfil: profile.photos[0]?.value,
           emailVerificado: true, // Google garantiza el email
